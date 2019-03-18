563b5dbb65e59e5c5867b91254b462f75635c25f
pewsonyaw-computew-uwu <victorianope@gmail.com>
2019-03-18 11:20:04 +0100

    Se añade lo que hay que obtener

--- scrapping.py
+++ scrapping.py
@@ -6,3 +6,17 @@ with open("ejemplo.html") as fp:
 soup.prettify()
 
 print(soup.title.string)
+
+# Cosas a hacer webscrapping:
+#   Busqueda:
+#       - Mediante tipo de carrera --> Grado
+#       - Mediante comunidad autonoma
+#   Resultados muestran:
+#       -  Tipo de grado
+#       - Nombre Universidad
+#       - Nota de Corte
+#       - Localizacion de la Universidad
+#       - Web de la Univesidad
+#       - Duracion
+#       - Universidad publica/privada
+#       - Precio del primer curso
