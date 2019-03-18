05b3b313781e8411c368b17d53676fbdfbcda9df
Daniel Ranchal <danitico98@gmail.com>
2019-03-18 11:47:36 +0100

    Ive added the names of bachelor's degree

--- scrapping.py
+++ scrapping.py
@@ -3,6 +3,7 @@ import bs4
 with open("ejemplo.html") as fp:
     soup = bs4.BeautifulSoup(fp, features="html.parser")
 
-soup.prettify()
 
-print(soup.title.string)
+# nombre de grados
+for link in soup.find_all(title='Pincha para más info sobre esta titulación'):
+    print(link.string)
