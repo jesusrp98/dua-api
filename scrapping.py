e286c6be472ccd74f6b583145ed5605a3867e516
Daniel Ranchal <danitico98@gmail.com>
2019-03-18 13:00:35 +0100

    Added status and uni

--- scrapping.py
+++ scrapping.py
@@ -5,19 +5,21 @@ with open("ejemplo.html") as fp:
 
 
 # nombre de grados
-for link in soup.find_all(title='Pincha para más info sobre esta titulación'):
-    print(link.string)
+grados = soup.find_all(title='Pincha para más info sobre esta titulación')
+universidad = soup.find_all(title='Pincha para más titulaciónes de esta universidad')
+tipo_universidad = soup.find_all(class_='label label-primary')
+
 
 # Cosas a hacer webscrapping:
 #   Busqueda:
 #       - Mediante tipo de carrera --> Grado
 #       - Mediante comunidad autonoma
 #   Resultados muestran:
-#       -  Tipo de grado
-#       - Nombre Universidad
+#       -  Tipo de grado Hecho
+#       - Nombre Universidad Hecho
 #       - Nota de Corte
 #       - Localizacion de la Universidad
 #       - Web de la Univesidad
 #       - Duracion
-#       - Universidad publica/privada
+#       - Universidad publica/privada Hecho
 #       - Precio del primer curso
