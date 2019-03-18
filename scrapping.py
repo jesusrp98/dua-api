1f3c6fd34988700f7078b682191165c13b20cb13
Victoriano Pedrajas <36993779+V1ckyuwu@users.noreply.github.com>
2019-03-18 14:13:01 +0100

    Arreglado el formato

--- scrapping.py
+++ scrapping.py
@@ -14,23 +14,11 @@ with open("ejemplo.html") as fp:
 #       - Nombre Universidad HECHO
 #       - Universidad publica/privada HECHO
 #       - Nota de Corte HECHO
-#       - Duracion
+#       - Duracion HECHO
 #       - Localizacion de la Universidad HECHO
 #       - Web de la Univesidad 
-#       - Precio del primer curso
+#       - Precio del primer curso HECHO
 
-<<<<<<< HEAD
-
-#funcion que imprime unicamente la nota de corte
-for link in soup.find_all(class_="titul-list-nota-corte-nota"):
-    print(link.string)
-
-#funcion que imprime unicamente a duracion de los grados
-Duracion= soup.find_all(class_="views-field views-field-field-duracion")
-for Duracion_ in Duracion:
-    Duracion_=Duracion_.find(class_="field-content")
-    print(Duracion_.string)
-=======
 # Nombre de grados
 grados = soup.find_all(title='Pincha para más info sobre esta titulación')
 #Nombre universidad
@@ -46,10 +34,10 @@ for Duracion_ in Duracion:
 #Localizacion de la Universidad
 localizacion = soup.find_all(class_="titul-list-provincia text-right")
 #Web de la Universidad
-aux = soup.find_all(class_="titul-list-provincia text-right")
-web = aux.get('href')
+aux = soup.find_all(class_="Pincha para más info sobre esta titulación")
+for web in aux
+    web = web.find('href').string
 #Precio del primer curso
 precios = soup.find_all(class_='views-field views-field-field-precio-primer-ano')
 for precio in precios:
     precio = precio.find(class_='field-content').string
->>>>>>> 6c7720a5c94b183944ee41655a60f0105a3a3f0c
