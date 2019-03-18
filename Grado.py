7bf3dfd5b73eb44a27fa30e11fd0d7d9363aa701
Daniel Ranchal <danitico98@gmail.com>
2019-03-18 19:25:50 +0100

    heheheh

---
+++ Grado.py
@@ -0,0 +1,11 @@
+import json
+class Resultado:
+    def __init__(self, grado, universidad, notas_corte, duracion, localizacion, web, precio):
+        self.grade = grado
+        self.university = universidad
+        self.mark = notas_corte
+        self.duration = duracion
+        self.location = localizacion
+        self.web = web
+        self.price = precio
+
