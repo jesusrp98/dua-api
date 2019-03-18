f3a9ac920bc5c08c48295ee322f3afdb9e5f6545
pewsonyaw-computew-uwu <victorianope@gmail.com>
2019-03-18 13:17:03 +0100

    El enlace web creo que funciona

--- scrapping.py
+++ scrapping.py
@@ -10,13 +10,13 @@ with open("ejemplo.html") as fp:
 #       - Mediante tipo de carrera --> Grado
 #       - Mediante comunidad autonoma
 #   Resultados muestran:
-#       - Tipo de grado HECHO
+#       - Nombre de grado HECHO
 #       - Nombre Universidad HECHO
-#       - Nota de Corte HECHO
-#       - Localizacion de la Universidad
-#       - Web de la Univesidad
-#       - Duracion
 #       - Universidad publica/privada HECHO
+#       - Nota de Corte HECHO
+#       - Duracion
+#       - Localizacion de la Universidad HECHO
+#       - Web de la Univesidad 
 #       - Precio del primer curso
 
 # Nombre de grados
@@ -29,3 +29,9 @@ tipo_universidad = soup.find_all(class_='label label-primary')
 nota_corte = soup.find_all(class_="titul-list-nota-corte-nota")
 #Duracion del grado
 for link in soup.find_all
+#Localizacion de la Universidad
+localizacion = soup.find_all(class_="titul-list-provincia text-right")
+#Web de la Universidad
+aux = soup.find_all(class_="titul-list-provincia text-right")
+web = aux.get('href')
+#Precio del primer curso
