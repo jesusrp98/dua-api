f61caeb417e10c3fee4e6e75f1b8c531f44f5ae6
Daniel Ranchal <danitico98@gmail.com>
2019-03-18 14:25:38 +0100

    Listifying haha

--- scrapping.py
+++ scrapping.py
@@ -21,23 +21,41 @@ with open("ejemplo.html") as fp:
 
 # Nombre de grados
 grados = soup.find_all(title='Pincha para más info sobre esta titulación')
+grados_lista = list()
+for grado in grados:
+    grados_lista.append(grado)
 #Nombre universidad
-universidad = soup.find_all(title='Pincha para más titulaciónes de esta universidad')
+universidades = soup.find_all(title='Pincha para más titulaciónes de esta universidad')
+universidad_lista = list()
+for universidad in universidades:
+    universidad_lista.append(universidad)
 #Tipo de universidad
-tipo_universidad = soup.find_all(class_='label label-primary')
+tipo_universidades = soup.find_all(class_='label label-primary')
+tipo_universidades_lista = list()
+for tipo_universidad in universidades:
+    tipo_univerdidades_lista.append(tipo_universidad)
 #Nota de corte
-nota_corte = soup.find_all(class_="titul-list-nota-corte-nota")
+notas_corte = soup.find_all(class_="titul-list-nota-corte-nota")
+notas_corte_lista = list()
+for nota in notas_corte:
+	notas_corte_lista.append(nota)
 #Duracion del grado
 Duracion = soup.find_all(class_="views-field views-field-field-duracion")
+duracion_lista = list()
 for Duracion_ in Duracion:
-    Duracion_ = Duracion_.find(class_="field-content")
+    duracion_lista.append(Duracion_.find(class_="field-content").string)
 #Localizacion de la Universidad
-localizacion = soup.find_all(class_="titul-list-provincia text-right")
+localizaciones = soup.find_all(class_="titul-list-provincia text-right")
+localizaciones_list = list()
+for localizacion in localizaciones:
+	localizaciones_list.append(localizacion)
 #Web de la Universidad
-aux = soup.find_all(class_="Pincha para más info sobre esta titulación")
-for web in aux
-    web = web.find('href').string
+webs = soup.find_all(class_="Pincha para más info sobre esta titulación")
+web_lista = list()
+for web in webs:
+	web_lista.append(web.find('href').string)
 #Precio del primer curso
 precios = soup.find_all(class_='views-field views-field-field-precio-primer-ano')
+precios_list = list()
 for precio in precios:
-    precio = precio.find(class_='field-content').string
+    precios_list.append(precio.find(class_='field-content').string)
